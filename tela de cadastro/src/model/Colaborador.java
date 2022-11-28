228e2bbdb079d4bcd8d17c8901da62104cdc7fda
Kenzo <lucaskenzok@outlook.com>
2022-11-28 16:01:08 -0300

    Commit AF

---
+++ tela de cadastro/src/model/Colaborador.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Colaborador {
+//Atributos
+public String nome;
+public String senha;
+public String email;
+//GET AND SET
+
+public String getNome(){
+return nome;    
+}
+public void setNome(String nome){
+this.nome = nome;    
+}
+public String getSenha(){
+return senha;    
+}
+public void setSenha(String senha){
+this.senha = senha;    
+}
+public String getEmail(){
+return email;    
+}
+public void setEmail(String email){
+this.email = email;
+}
+}
